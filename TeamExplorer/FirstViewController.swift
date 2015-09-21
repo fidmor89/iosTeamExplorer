6013c79282378486fd1fd440523cac170d6d65fa
Manuel <manu1217@outlook.com>
2015-09-20 18:48:46 -0600

    It have in the story board the interface for work and is prepared the table for the add of data so it can change dimension automatically.

--- TeamExplorer/FirstViewController.swift
+++ TeamExplorer/FirstViewController.swift
@@ -13,13 +13,45 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // navigationItem.title = "One"
+        navigationItem.title = "alsdfsdf"
+    }
+    @IBAction func Prueba(sender: UIButton) {
+        var refreshAlert = UIAlertController(title: "Instrucciones:", message: "Por favor seleccione un servidor para trabajar", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
+            println("Handle Ok logic here")
+        }))
+        
+       
+        
+        presentViewController(refreshAlert, animated: true, completion: nil)
+
     }
 
+    @IBAction func BackButton(sender: UIBarButtonItem) {
+        
+        var refreshAlert = UIAlertController(title: "Instrucciones:", message: "Ud ira hacia atras en la aplicación", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
+            println("Handle Ok logic here")
+        }))
+        
+        
+        
+        presentViewController(refreshAlert, animated: true, completion: nil)
+    }
+    
 
 }
 
